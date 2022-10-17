886ca3d8b681135194561f13cd6dda9185b007cc
shubhamgite <shubhamgite1106@gmail.com>
2022-10-17 16:30:44 +0530

    creating repo for ecom website

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 9324746165</label>
+            <i className='fa fa-envelope'></i>
+            <label> shubhu1346@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span></span>
+            <label>IN</label>
+            <span></span>
+            <label>Rupee </label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
